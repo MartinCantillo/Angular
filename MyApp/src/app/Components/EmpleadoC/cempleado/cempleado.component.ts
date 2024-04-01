698cfff720417750f13b6f353a6532d0e44fac8f
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-04-01 17:39:54 -0500

    NgFor

--- MyApp/src/app/Components/EmpleadoC/cempleado/cempleado.component.ts
+++ MyApp/src/app/Components/EmpleadoC/cempleado/cempleado.component.ts
@@ -10,23 +10,33 @@ export class CEmpleadoComponent {
   Apellido = "Cantillo";
   Cargo = "Ingeniero de Softwate";
   Edad = 21;
+  //mapas
+  employes: Empleado[];
   //Porperty  Binding , la propiedad del html tiene que ir entre[] para que lo reconozca esta propiedad
   habilitacioncuadro = false;
-  user="Usuario no registrado";
+  user = "Usuario no registrado";
+  constructor() {
+    this.employes = [{Cargos: "Ingeniero de software"},{Cargos: "Arquitecto de software"}];
+}
+//event binding
+registrar() {
+  //alert("El usuario se acaba de registrar");
+  this.user = "El usuario se acaba de registrar"
+}
 
-  //event binding
-  registrar(){
-    //alert("El usuario se acaba de registrar");
-    this.user="El usuario se acaba de registrar"
-  }
+//two way binging -- Gnmodul
+empresaNgModel = 'default value';
 
-  //two way binging -- Gnmodul
-  empresaNgModel='default value';
+//NgIf
+registrado = false;
 
-  //NgIf
-  registrado=false;
 
-  userRegistro(){
-    this.registrado=true;
-  }
+userRegistro() {
+  this.registrado = true;
+}
+
+}
+
+export interface Empleado {
+  Cargos: string;
 }
